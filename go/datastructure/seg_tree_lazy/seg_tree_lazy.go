6eb1f9360ad5e014f3ee4aa63e4b3e41bfd9ea9f
salmonfishycooked <salmonfishycooked@gmail.com>
2023-07-14 22:59:17 +0800

    add lazy segment tree

---
+++ go/datastructure/seg_tree_lazy/seg_tree_lazy.go
@@ -0,0 +1,120 @@
+package seg_tree_lazy
+
+// NOTE:
+// the example lazy segment tree records sum of [l, r)
+// the function UpdateArea() is used by increasing original values by v
+
+type SegTreeLazy struct {
+	ele []segnode
+	ori []int
+}
+
+type segnode struct {
+	l, r  int
+	v     int
+	lazyv int
+	lazy  bool
+}
+
+func NewSegTreeLazy(a []int) *SegTreeLazy {
+	n := len(a)
+	tree := &SegTreeLazy{
+		ele: make([]segnode, 4*n),
+		ori: a,
+	}
+	tree.build(0, 0, n)
+	return tree
+}
+
+// build a segment tree
+// node k saves the information of [l, r)
+func (t *SegTreeLazy) build(k, l, r int) {
+	t.ele[k].l, t.ele[k].r = l, r
+	if l+1 == r {
+		t.ele[k].v = t.ori[l]
+		return
+	}
+	mid := (l + r) >> 1
+	t.build(t.leftChild(k), l, mid)
+	t.build(t.rightChild(k), mid, r)
+	t.updateNode(k)
+}
+
+func (t *SegTreeLazy) leftChild(k int) int {
+	return 2*k + 1
+}
+
+func (t *SegTreeLazy) rightChild(k int) int {
+	return 2*k + 2
+}
+
+// query, the main logic
+
+// UpdateArea changes value of [l, r)
+// using lazy flag
+func (t *SegTreeLazy) UpdateArea(l, r, v int) {
+	t.updateArea(0, l, r, v)
+}
+
+func (t *SegTreeLazy) updateArea(k, l, r, v int) {
+	if t.ele[k].l == l && t.ele[k].r == r {
+		t.lazy(k, v)
+		return
+	}
+	if t.ele[k].lazy {
+		t.lazyDown(k)
+	}
+	mid := (t.ele[k].l + t.ele[k].r) >> 1
+	lc, rc := t.leftChild(k), t.rightChild(k)
+	if r <= mid {
+		t.updateArea(lc, l, r, v)
+	} else if l >= mid {
+		t.updateArea(rc, l, r, v)
+	} else {
+		t.updateArea(lc, l, mid, v)
+		t.updateArea(rc, mid, r, v)
+	}
+	t.updateNode(k)
+}
+
+func (t *SegTreeLazy) updateNode(k int) {
+	t.ele[k].v = t.ele[t.leftChild(k)].v + t.ele[t.rightChild(k)].v
+}
+
+// lazy makes the node k being lazy
+func (t *SegTreeLazy) lazy(k, v int) {
+	t.ele[k].lazy = true
+	t.ele[k].lazyv += v
+	t.ele[k].v += (t.ele[k].r - t.ele[k].l) * v
+}
+
+// lazyDown clears lazy flag of the node k and passes it to its children
+func (t *SegTreeLazy) lazyDown(k int) {
+	t.lazy(t.leftChild(k), t.ele[k].lazyv)
+	t.lazy(t.rightChild(k), t.ele[k].lazyv)
+	t.ele[k].lazy = false
+	t.ele[k].lazyv = 0
+}
+
+// Query returns the sum of [l, r)
+// using lazy flag
+func (t *SegTreeLazy) Query(l, r int) int {
+	return t.query(0, l, r)
+}
+
+func (t *SegTreeLazy) query(k, l, r int) int {
+	if t.ele[k].l == l && t.ele[k].r == r {
+		return t.ele[k].v
+	}
+	if t.ele[k].lazy {
+		t.lazyDown(k)
+	}
+	mid := (t.ele[k].l + t.ele[k].r) >> 1
+	if r <= mid {
+		return t.query(t.leftChild(k), l, r)
+	}
+	if l >= mid {
+		return t.query(t.rightChild(k), l, r)
+	}
+	return t.query(t.leftChild(k), l, mid) + t.query(t.rightChild(k), mid, r)
+}
---
+++ go/datastructure/seg_tree_lazy/seg_tree_lazy_test.go
@@ -0,0 +1,33 @@
+package seg_tree_lazy
+
+import (
+	"testing"
+)
+
+var a = []int{5, 7, 2, 12, 9, 5, 13, 15}
+
+func TestSegTreeLazy_Query(t *testing.T) {
+	seg := NewSegTreeLazy(a)
+	if v := seg.Query(0, 3); v != 14 {
+		t.Log("In TestSegTree_Query(): the result of seg.Query(0, 3) should be 14, but got", v)
+		t.FailNow()
+	}
+	if v := seg.Query(0, 8); v != 68 {
+		t.Log("In TestSegTree_Query(): the result of seg.Query(0, 8) should be 68, but got", v)
+		t.FailNow()
+	}
+}
+
+func TestSegTreeLazy_UpdateArea(t *testing.T) {
+	seg := NewSegTreeLazy(a)
+	seg.UpdateArea(0, 3, 5)
+	if v := seg.Query(0, 3); v != 29 {
+		t.Log("In TestSegTree_Update(): the result of seg.Query(0, 3) should be 29, but got", v)
+		t.FailNow()
+	}
+	seg.UpdateArea(1, 3, 2)
+	if v := seg.Query(0, 8); v != 87 {
+		t.Log("In TestSegTree_Update(): the result of seg.Query(0, 8) should be 79, but got", v)
+		t.FailNow()
+	}
+}
